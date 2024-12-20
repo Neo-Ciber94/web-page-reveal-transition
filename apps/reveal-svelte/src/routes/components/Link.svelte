b84df7acd72a737aa0c652c7de064fbf4a41ec2e
Freddy L <neo-ciber_94@hotmail.com>
2024-12-19 22:28:58 -0400

    minor changes

--- apps/reveal-svelte/src/routes/components/Link.svelte
+++ apps/reveal-svelte/src/routes/components/Link.svelte
@@ -5,7 +5,7 @@
 
 	let { href, children }: { href: string; children: Snippet } = $props();
 
-	const isActive = new URL(href, 'http://a').pathname === page.url.pathname;
+	const isActive = $derived(new URL(href, 'http://a').pathname === page.url.pathname);
 
 	function handleClick(ev: Event) {
 		const target = ev.currentTarget as HTMLAnchorElement;
